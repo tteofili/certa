5d7a0f6502d7d43b746b7887b7e74b6192898565
Tommaso Teofili <tteofili@redhat.com>
2021-02-02 08:51:34 +0100

    added eval

---
+++ certa/eval.py
@@ -0,0 +1,51 @@
+import random
+
+import numpy as np
+import pandas as pd
+
+
+def expl_eval(class_to_explain, explanation_attributes, e_score, lsource, l_record, model, prediction, rsource,
+              r_record, predict_fn):
+    results = []
+    if len(explanation_attributes) > 0:
+        for i in range(2):
+            lt = l_record.copy()
+            rt = r_record.copy()
+            perturb = 'sample'
+            for e in explanation_attributes:
+                if len(e) > 0:
+                    if i == 0:
+                        perturb = 'drop'
+                        lt[e] = ''
+                        rt[e] = ''
+                    else:
+                        f_class = abs(class_to_explain - 1)
+                        if 'class' in lsource.columns:
+                            ls_flip = lsource[lsource['class'] == f_class]
+                            rs_flip = rsource[rsource['class'] == f_class]
+                        else:
+                            ls_flip = lsource.copy()
+                            rs_flip = rsource.copy()
+                        perturb = 'sample'
+                        randint = random.randint(0, len(ls_flip) - 1)
+                        lt[e] = ls_flip.iloc[randint][e]
+                        randint = random.randint(0, len(rs_flip) - 1)
+                        rt[e] = rs_flip.iloc[randint][e]
+            df = pd.DataFrame(lt.add_prefix('ltable_').append(rt.add_prefix('rtable_'))).transpose()
+            df = df.drop([c for c in ['ltable_id', 'rtable_id'] if c in df.columns], axis=1)
+            modified_tuple_prediction = predict_fn(df, model)[['nomatch_score', 'match_score']].values[0]
+            modified_class = np.argmax(modified_tuple_prediction)
+            flip = abs(modified_class - class_to_explain)
+            class_probability = prediction[class_to_explain]
+            drop = class_probability - modified_tuple_prediction[class_to_explain]
+            n_drop = (class_probability - modified_tuple_prediction[class_to_explain]) / class_probability
+            impact = flip or (modified_tuple_prediction[class_to_explain] < 0.5 * class_probability)
+            n_impact = flip or (
+                    modified_tuple_prediction[class_to_explain] / class_probability < 0.5 * class_probability)
+            ids = str(l_record['id']) + '-' + str(r_record['id'])
+            new_row = {'match': class_to_explain, 'e_score': e_score, 'drop': drop, 'n_drop': n_drop,
+                       'perturb': perturb, 'flip': flip, 'impact': impact, 'n_impact': n_impact,
+                       'attributes': explanation_attributes,
+                       'e_size': len(explanation_attributes), 'prediction': class_probability, 'row': ids}
+            results.append(new_row)
+    return pd.DataFrame(results)
--- example_deeper.py
+++ example_deeper.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
+import os
+import gensim.downloader as api
+import models.DeepER as dp
+from certa.local_explain import find_similarities
+from certa.local_explain import dataset_local
+from certa.triangles_method import explainSamples
+from certa.eval import expl_eval
 
-datadir = 'datasets/beers/'
-lsource = pd.read_csv(datadir + 'tableA.csv')
-rsource = pd.read_csv(datadir + 'tableB.csv')
-gt = pd.read_csv(datadir + 'train.csv')
-valid = pd.read_csv(datadir + 'valid.csv')
-test = pd.read_csv(datadir + 'test.csv')
 
 def merge_sources(table, left_prefix, right_prefix, left_source, right_source, copy_from_table, ignore_from_table):
     dataset = pd.DataFrame(columns={col: table[col].dtype for col in copy_from_table})
@@ -19,8 +20,7 @@ def merge_sources(table, left_prefix, right_prefix, left_source, right_source, c
         new_row = {column: row[column] for column in copy_from_table}
 
         try:
-            for id, source, prefix in [(leftid, left_source, left_prefix),
-                                       (rightid, right_source, right_prefix)]:
+            for id, source, prefix in [(leftid, left_source, left_prefix), (rightid, right_source, right_prefix)]:
 
                 for column in source.keys():
                     if column not in ignore_column:
@@ -30,6 +30,7 @@ def merge_sources(table, left_prefix, right_prefix, left_source, right_source, c
         except:
             pass
     return dataset
+
 
 def to_deeper_data(df: pd.DataFrame):
     res = []
@@ -44,25 +45,6 @@ def to_deeper_data(df: pd.DataFrame):
             res.append((lpd.values.astype('str'), rpd.values.astype('str')))
     return res
 
-train_df = merge_sources(gt, 'ltable_', 'rtable_', lsource, rsource, ['label'], ['id'])
-valid_df = merge_sources(valid, 'ltable_', 'rtable_', lsource, rsource, ['label'], ['id'])
-test_df = merge_sources(test, 'ltable_', 'rtable_', lsource, rsource, ['label'], ['id'])
-
-import os
-import gensim.downloader as api
-
-if not os.path.exists('models/glove.6B.50d.txt'):
-    word_vectors = api.load("glove-wiki-gigaword-50")
-    word_vectors.save_word2vec_format('models/glove.6B.50d.txt', binary=False)
-
-import models.DeepER as dp
-
-embeddings_index = dp.init_embeddings_index('models/glove.6B.50d.txt')
-emb_dim = len(embeddings_index['cat'])
-embeddings_model, tokenizer = dp.init_embeddings_model(embeddings_index)
-model = dp.init_DeepER_model(emb_dim)
-
-model = dp.train_model_ER(to_deeper_data(train_df), model, embeddings_model, tokenizer)
 
 def predict_fn(x, m, ignore_columns=['ltable_id', 'rtable_id', 'label']):
     data = to_deeper_data(x.drop([c for c in ignore_columns if c in x.columns], axis=1))
@@ -70,6 +52,7 @@ def predict_fn(x, m, ignore_columns=['ltable_id', 'rtable_id', 'label']):
     out_df = pd.DataFrame(out, columns=['nomatch_score', 'match_score'])
     out_df.index = x.index
     return pd.concat([x.copy(), out_df], axis=1)
+
 
 def get_original_prediction(r1, r2):
     lprefix = 'ltable_'
@@ -83,23 +66,46 @@ def get_original_prediction(r1, r2):
     r1r2 = r1r2.drop([lprefix + 'id', rprefix + 'id'], axis=1)
     return predict_fn(r1r2, model)[['nomatch_score', 'match_score']].values[0]
 
-from certa.local_explain import find_similarities
+
+datadir = 'datasets/beers/'
+lsource = pd.read_csv(datadir + 'tableA.csv')
+rsource = pd.read_csv(datadir + 'tableB.csv')
+gt = pd.read_csv(datadir + 'train.csv')
+valid = pd.read_csv(datadir + 'valid.csv')
+test = pd.read_csv(datadir + 'test.csv')
+
+train_df = merge_sources(gt, 'ltable_', 'rtable_', lsource, rsource, ['label'], ['id'])
+valid_df = merge_sources(valid, 'ltable_', 'rtable_', lsource, rsource, ['label'], ['id'])
+test_df = merge_sources(test, 'ltable_', 'rtable_', lsource, rsource, ['label'], ['id'])
+
+if not os.path.exists('models/glove.6B.50d.txt'):
+    word_vectors = api.load("glove-wiki-gigaword-50")
+    word_vectors.save_word2vec_format('models/glove.6B.50d.txt', binary=False)
+
+embeddings_index = dp.init_embeddings_index('models/glove.6B.50d.txt')
+emb_dim = len(embeddings_index['cat'])
+embeddings_model, tokenizer = dp.init_embeddings_model(embeddings_index)
+model = dp.init_DeepER_model(emb_dim)
+
+model = dp.train_model_ER(to_deeper_data(train_df), model, embeddings_model, tokenizer)
 
 theta_min, theta_max = find_similarities(test_df, False)
 
-from certa.local_explain import dataset_local
-from certa.triangles_method import explainSamples
-import math
+l_tuple = lsource.iloc[19]
+r_tuple = rsource.iloc[27]
+local_samples = dataset_local(l_tuple, r_tuple, model, lsource, rsource, datadir, theta_min, theta_max, predict_fn,
+                              num_triangles=10)
 
-nt = int(math.log(len(lsource) * len(rsource)))
-
-l_tuple = lsource.iloc[0]
-r_tuple = rsource.iloc[0]
-local_samples = dataset_local(l_tuple, r_tuple, model, lsource, rsource, datadir,
-                              theta_min, theta_max, predict_fn, num_triangles=nt)
-
-class_to_explain = np.argmax(get_original_prediction(l_tuple, r_tuple))
+prediction = get_original_prediction(l_tuple, r_tuple)
+class_to_explain = np.argmax(prediction)
 
 explanation, flipped_pred = explainSamples(local_samples, [lsource, rsource], model, predict_fn,
                                            class_to_explain=class_to_explain, maxLenAttributeSet=3)
 print(explanation)
+
+for exp in explanation:
+    e_attrs = exp.split('/')
+    e_score = explanation[exp]
+    expl_evaluation = expl_eval(class_to_explain, e_attrs, e_score, lsource, l_tuple, model, prediction, rsource,
+                                r_tuple, predict_fn)
+    print(expl_evaluation.head())
