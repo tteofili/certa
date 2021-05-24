73ca9d61aa444c6038597292cb36df6a8287239c
tteofili <tommaso.teofili@gmail.com>
2021-05-24 08:46:30 +0200

    minor adjustments

--- eval.py
+++ eval.py
@@ -1,10 +1,8 @@
-#deeper
 from eval_deeper import eval_deeper
 from eval_dm import eval_dm
 from eval_emt import eval_emt
 
-filtered_datasets = ['dirty_dblp_scholar', 'dirty_amazon_itunes', 'dirty_walmart_amazon',
-                                           'dirty_dblp_acm']
+filtered_datasets = ['dirty_dblp_scholar', 'dirty_amazon_itunes', 'dirty_walmart_amazon', 'dirty_dblp_acm']
 evals_list_deeper = eval_deeper(filtered_datasets=filtered_datasets)
 evals_list_dm = eval_dm(filtered_datasets=filtered_datasets)
 evals_list_emt = eval_emt(filtered_datasets=filtered_datasets)
