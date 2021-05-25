f7fd85bf375a7567a4c8343d70cff8bd97e1e148
tteofili <tommaso.teofili@gmail.com>
2021-05-25 11:39:06 +0200

    adding missing datasets

--- eval.py
+++ eval.py
@@ -3,7 +3,7 @@ from eval_dm import eval_dm
 from eval_emt import eval_emt
 
 samples = 10
-filtered_datasets = ['dirty_dblp_scholar', 'dirty_amazon_itunes', 'dirty_walmart_amazon', 'dirty_dblp_acm']
+filtered_datasets = ['abt_buy', 'dirty_dblp_scholar', 'dirty_amazon_itunes', 'dirty_walmart_amazon', 'dirty_dblp_acm']
 #evals_list_deeper = eval_deeper(filtered_datasets=filtered_datasets)
-evals_list_dm = eval_dm(samples=samples, filtered_datasets=filtered_datasets)
+#evals_list_dm = eval_dm(samples=samples, filtered_datasets=filtered_datasets)
 evals_list_emt = eval_emt(samples=samples, filtered_datasets=filtered_datasets)
