c57cf8d9d8a236e94f544d4f0ae1823621681315
Tommaso Teofili <tteofili@redhat.com>
2021-06-08 11:34:58 +0200

    minor fixes

--- certa/explain.py
+++ certa/explain.py
@@ -6,12 +6,13 @@ from certa.local_explain import get_original_prediction
 
 
 def explain(l_tuple, r_tuple, lsource, rsource, predict_fn, dataset_dir, fast: bool = True, left=True, right=True,
-            combinations=True, check=False, discard_bad=False):
+            combinations=True, check=False, discard_bad=False, num_triangles=100, return_top=False):
     predicted_class = np.argmax(get_original_prediction(l_tuple, r_tuple, predict_fn))
     local_samples, gleft_df, gright_df = local_explain.dataset_local(l_tuple, r_tuple, lsource, rsource,
                                                                      predict_fn, class_to_explain=predicted_class,
                                                                      datadir=dataset_dir, use_predict=not fast,
-                                                                     use_w=right, use_y=left)
+                                                                     use_w=right, use_y=left,
+                                                                     num_triangles=num_triangles)
 
     if combinations:
         maxLenAttributeSet = len(l_tuple) - 2
@@ -21,6 +22,6 @@ def explain(l_tuple, r_tuple, lsource, rsource, predict_fn, dataset_dir, fast: b
                                                                         pd.concat([rsource, gleft_df])],
                                                         predict_fn, predicted_class,
                                                         check=check, discard_bad=discard_bad,
-                                                        maxLenAttributeSet=maxLenAttributeSet)
+                                                        maxLenAttributeSet=maxLenAttributeSet, return_top=return_top)
     return explanation, flipped, triangles
 
