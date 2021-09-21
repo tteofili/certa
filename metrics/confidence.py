c8cc5f1109d3fbe0f15edaf67871b807039b0bb1
Tommaso Teofili <tteofili@redhat.com>
2021-09-21 14:27:20 +0200

    fixed confidence

--- metrics/confidence.py
+++ metrics/confidence.py
@@ -5,6 +5,7 @@ import os
 import random
 from collections import defaultdict
 
+import pandas as pd
 import numpy as np
 from scipy.special import softmax
 from sklearn.linear_model import LinearRegression
@@ -44,129 +45,126 @@ def sample(X, y, mode='up'):
     return X[new_idx], y[new_idx]
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--models_dir",
-                        help="Path where the models can be found",
-                        default='snli_bert', type=str)
-    parser.add_argument("--saliency_dir",
-                        help="Direcotry where saliencies are serialized",
-                        type=str)
-    parser.add_argument("--saliency", help="Saliency name", nargs='+')
-    parser.add_argument("--upsample", choices=['up', 'none'])
-
-    args = parser.parse_args()
+def get_confidence(base_dir: str):
     np.random.seed(1)
-
-    print(args, flush=True)
+    saliency_names = ['certa', 'landmark', 'mojito_c', 'mojito_d', 'shap']
     all_y = []
-    for saliency in args.saliency:
+    examples_df = pd.read_csv(os.path.join(base_dir, 'examples.csv'))
+    for saliency in saliency_names:
         print(saliency)
         test_scores = []
-        test_coefs = []
-        for model_path in os.listdir(args.models_dir):
 
-            if model_path.endswith('.predictions'):
-                continue
-            print(model_path)
+        print(base_dir)
 
-            full_model_path = os.path.join(args.models_dir, model_path)
-            predictsions_path = full_model_path + '.predictions'
-            saliency_path = os.path.join(args.saliency_dir,
-                                         f"{model_path}_{saliency}")
+        # full_model_path = os.path.join(args.models_dir, model_path)
+        # predictsions_path = full_model_path + '.predictions'
+        saliency_path = os.path.join(base_dir,
+                                     f"{saliency}.csv")
 
-            predictions = json.load(open(predictsions_path))
-            class_preds = predictions['class']
-            logits = predictions['logits']
-            saliencies = []
-            all_confidences = []
-            all_saliencies = []
-            classes = [0, 1, 2]
-            if 'imdb' in args.saliency_dir:
-                classes = [0, 1]
-            features = []
-            y = []
-            tokens = []
+        saliency_df = pd.read_csv(os.path.join(base_dir, saliency + '.csv'))
 
-            with open(saliency_path) as out:
-                for i, line in enumerate(out):
-                    try:
-                        instance_saliency = json.loads(line)
-                    except:
-                        continue
+        predictions = saliency_df['prediction']
+        class_preds = predictions.apply(lambda x: np.argmax(x))
+        logits = predictions.copy()
+        saliencies = []
+        classes = [0, 1]
+        features = []
+        y = []
+        tokens = []
 
-                    instance_sals = []
-                    instance_tokens = []
-                    for _cls in classes:
-                        cls_sals = []
-                        for _token in instance_saliency['tokens']:
-                            if _cls == 0:
-                                instance_tokens.append(_token['token'])
-                            if _token['token'] == '[PAD]':
-                                break
-                            cls_sals.append(_token[str(_cls)])
-                        instance_sals.append(cls_sals)
-                    saliencies.append(instance_sals)
-                    tokens.append(instance_tokens)
+        for i in range(len(saliency_df)):
+            instance_saliency = saliency_df.iloc[i]
+            instance_sals = []
+            instance_tokens = []
+            for _cls in classes:
+                cls_sals = []
+                explanation = instance_saliency['explanation']
+                attributes_dict = json.loads(explanation.replace("'", "\""))
+                for _token, sal in attributes_dict.items():
+                    if _cls == 0:
+                        instance_tokens.append(_token)
+                    if saliency == 'certa':
+                        if _cls == class_preds[i]:
+                            attr_sal = sal
+                        else:
+                            attr_sal = 1 - sal
+                    else:
+                        if _cls == 0:
+                            if sal < 0:
+                                attr_sal = abs(sal)
+                            else:
+                                attr_sal = 0
+                        else:
+                            if sal > 0:
+                                attr_sal = sal
+                            else:
+                                attr_sal = 0
+                    cls_sals.append(attr_sal)
+                instance_sals.append(cls_sals)
+            saliencies.append(instance_sals)
+            tokens.append(instance_tokens)
 
-            for i, instance in enumerate(saliencies):
-                _cls = class_preds[i]
-                instance_saliency = saliencies[i]
-                instance_logits = softmax(logits[i])
+        for i, instance in enumerate(saliencies):
+            _cls = class_preds[i]
+            instance_saliency = saliencies[i]
+            instance_logits = np.fromstring(logits[i].replace('[','').replace(']',''), dtype=float, sep=' ')
 
-                confidence_pred = instance_logits[_cls]
-                saliency_pred = np.array(instance_saliency[_cls])
+            confidence_pred = instance_logits[_cls]
+            saliency_pred = np.array(instance_saliency[_cls])
 
-                left_classes = classes.copy()
-                left_classes.remove(_cls)
-                other_sals = [np.array(instance_saliency[c_]) for c_ in
-                              left_classes]
-                feats = []
+            left_classes = classes.copy()
+            left_classes.remove(_cls)
+            other_sals = [np.array(instance_saliency[c_]) for c_ in
+                          left_classes]
+            feats = []
 
-                if len(classes) == 2:
-                    feats.append(sum(saliency_pred - other_sals[0]))
-                    feats.append(sum(saliency_pred - other_sals[0]))
-                    feats.append(sum(saliency_pred - other_sals[0]))
+            if len(classes) == 2:
+                feats.append(sum(saliency_pred - other_sals[0]))
+                feats.append(sum(saliency_pred - other_sals[0]))
+                feats.append(sum(saliency_pred - other_sals[0]))
 
-                else:
-                    feats.append(sum(np.max([saliency_pred - other_sals[0],
-                                             saliency_pred - other_sals[1]],
-                                            axis=0)))
-                    feats.append(sum(np.mean([saliency_pred - other_sals[0],
-                                              saliency_pred - other_sals[1]],
-                                             axis=0)))
-                    feats.append(sum(np.min([saliency_pred - other_sals[0],
-                                             saliency_pred - other_sals[1]],
-                                            axis=0)))
+            else:
+                feats.append(sum(np.max([saliency_pred - other_sals[0],
+                                         saliency_pred - other_sals[1]],
+                                        axis=0)))
+                feats.append(sum(np.mean([saliency_pred - other_sals[0],
+                                          saliency_pred - other_sals[1]],
+                                         axis=0)))
+                feats.append(sum(np.min([saliency_pred - other_sals[0],
+                                         saliency_pred - other_sals[1]],
+                                        axis=0)))
 
-                y.append(confidence_pred)
-                features.append(feats)
+            y.append(confidence_pred)
+            features.append(feats)
 
-            features = MinMaxScaler().fit_transform(np.array(features))
-            all_y += y
-            y = np.array(y)
+        features = MinMaxScaler().fit_transform(np.array(features))
+        all_y += y
+        y = np.array(y)
 
-            rs = ShuffleSplit(n_splits=5, random_state=2)
-            scores = []
-            coefs = []
-            for train_index, test_index in rs.split(features):
-                X_train, y_train, X_test, y_test = features[train_index], y[
-                    train_index], features[test_index], y[test_index]
-                if args.upsample == 'up':
-                    X_train, y_train = sample(X_train, y_train, mode='up')
-                reg = LinearRegression().fit(X_train, y_train)
-                pred = reg.predict(X_train)
-                test_pred = reg.predict(X_test)
+        rs = ShuffleSplit(n_splits=5, random_state=2)
+        scores = []
+        coefs = []
+        for train_index, test_index in rs.split(features):
+            X_train, y_train, X_test, y_test = features[train_index], y[
+                train_index], features[test_index], y[test_index]
+            reg = LinearRegression().fit(X_train, y_train)
+            pred = reg.predict(X_train)
+            test_pred = reg.predict(X_test)
 
-                all_metrics = []
-                for metric in [mean_absolute_error, max_error]:
-                    all_metrics.append(metric(y_test, test_pred))
-                scores.append(all_metrics)
-                coefs.append(reg.coef_)
+            all_metrics = []
+            for metric in [mean_absolute_error, max_error]:
+                all_metrics.append(metric(y_test, test_pred))
+            scores.append(all_metrics)
+            coefs.append(reg.coef_)
 
             test_scores.append([np.mean([_s[i] for _s in scores]) for i in
-                                range(len(scores[0]))])
+                            range(len(scores[0]))])
 
         print(' '.join([f"{np.mean([_s[l] for _s in test_scores]):.3f} "
-                        f"($\pm$ {np.std([_s[l] for _s in test_scores]):.3f})"
-                        for l in range(len(test_scores[0]))]), flush=True)
+                    f"($\pm$ {np.std([_s[l] for _s in test_scores]):.3f})"
+                    for l in range(len(test_scores[0]))]), flush=True)
+
+
+
+if __name__ == "__main__":
+    get_confidence('/home/tteofili/dev/certa/quantitative/beers/deeper')
