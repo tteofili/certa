0c57005ad3c6ddeb1e86dc15cd92aa7965778713
Tommaso Teofili <tteofili@redhat.com>
2021-09-23 14:38:27 +0200

    muted useless log

--- emt/torch_initializer.py
+++ emt/torch_initializer.py
@@ -29,7 +29,4 @@ def setup_gpu():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     n_gpu = torch.cuda.device_count()
 
-    logging.info("We use the device: '{}' and {} gpu's. Important: distributed and 16-bits training "
-                 "is currently not implemented! ".format(device, n_gpu))
-
     return device, n_gpu
