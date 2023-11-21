dadc800ed12c90ce5968e4a6948e995f46440dfc
Tommaso Teofili <tteofili@redhat.com>
2023-11-21 10:54:07 +0100

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="CERTA",
-    version="0.0.2",
+    version="0.0.3",
     author="Tommaso Teofili",
     author_email="tommaso.teofili@gmail.com",
     description="Computing ER explanations with TriAngles",
